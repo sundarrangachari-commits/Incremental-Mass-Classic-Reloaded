fb98650b97a4663421aed57d17cfe97b5979e3d0
MrRedShark77 <mrredshark77@gmail.com>
2024-10-08 22:01:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Incremental-Mass-Classic-Reloaded/',
   plugins: [
     vue2(),
     legacy({
